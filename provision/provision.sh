893d177c352a1979868500d5625bc875dccd0969
Richard King <richrdkng@gmail.com>
2016-03-14 15:43:06 +0100

    Add Vagrant settings to the project

---
+++ provision/provision.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# enter permanent su
+sudo su
+
+# update before provisioning
+apt-get update -y
+
+# install essentials
+apt-get install -y build-essential git nano curl mc
+
+# stop & disable apache
+service apache2 stop
+update-rc.d -f apache2 remove
+
+# install & start nginx
+apt-get install -y nginx
+service nginx start
+
+# configure nginx
+    # create initial www folder+content
+        # link /vagrant to /www (on the guest)
+        rm -rf /www
+        ln -s /vagrant /www
+
+    # copy configuration files
+    yes | cp -rf /vagrant/provision/etc/nginx/ /etc/
+
+    # restart service
+    service nginx reload
+
+# install node & update npm
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+apt-get install -y nodejs
+npm install npm -g
+
+# install gulp
+npm rm --global gulp
+npm install --global gulp gulp-cli
+
+# add custom .bashrc content to .bashrc
+cat /vagrant/provision/home/vagrant/.bashrc >> /home/vagrant/.bashrc
